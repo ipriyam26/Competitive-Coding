d0b3b0edc8af7db877c0ab92014a79fe4d3d90c1
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-12-29 17:32:05 +0530

    3 problems

---
+++ round-828-div3/second.py
@@ -0,0 +1,103 @@
+# Modules Start
+from __future__ import division, print_function
+import sys
+import os
+# import time
+from io import BytesIO, IOBase
+import math
+from collections import defaultdict
+# Modules End
+
+# Helpers
+INT_MAX = 2147483647
+INT_MIN = -2147483648
+mod = int(1.e9) + 7
+# Helpers End
+
+
+def main():
+    test_cases = int(input())
+    for _ in range(test_cases):
+        n,q = [int(num) for num in input().split(" ")]
+        arr = [int(num) for num in input().split(" ")]
+        sum_now = sum(arr)
+        even =0
+        odd = 0
+        for element in arr:
+            if element%2==0:
+                even+=1
+            else:
+                odd+=1
+        for _ in range(q):
+            choice,to_be_added = [int(num) for num in input().split(" ")]
+            sum_now += even*to_be_added if choice==0 else odd*to_be_added
+            print(sum_now)
+            if choice == 0 and to_be_added % 2 == 1:
+                odd+=even
+                even=0
+            if choice == 1 and to_be_added % 2 == 1:
+                even+=odd
+                odd=0
+                
+            
+
+
+
+
+# Settings
+sys.setrecursionlimit(10000000)
+# Settings End
+
+# region fast_io
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+# endregion
+
+
+if __name__ == '__main__':
+    main()
